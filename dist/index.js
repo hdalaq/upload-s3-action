4d97c40bd402e7350887a1f470754da0bc90ff55
Hosam <hdalaq@gmail.com>
2021-02-28 03:50:20 -0500

    simplify

--- dist/index.js
+++ dist/index.js
@@ -40,29 +40,6 @@ const destinationDir = DESTINATION_DIR === '/' ? shortid() : DESTINATION_DIR;
 const paths = klawSync(SOURCE_DIR, {
   nodir: true
 });
-const metadata = getMap(METADATA);
-
-function getMap(jsonString){
-  var map = JSON.parse(jsonString, (key, value) =>{
-    var map = new Map();
-    map.set(key, value);
-    return map
-  });
-
-  return map;
-}
-
-// function dataReviver(key, value)
-// { 
-//     if(key == 'lastname')
-//     {
-//         var newLastname = "test";
-//         return newLastname;
-//     }
-
-//   return value;  // < here is where un-modified key/value pass though
-
-// }
 
 function upload(params) {
   return new Promise(resolve => {
@@ -85,7 +62,7 @@ function run() {
         Bucket: BUCKET,
         ACL: 'public-read',
         Body: fileStream,
-        Metadata: metadata,
+        Metadata: METADATA,
         Key: bucketPath,
         ContentType: lookup(p.path) || 'text/plain'
       };
@@ -98,8 +75,7 @@ run()
   .then(locations => {
     core.info(`object key - ${destinationDir}`);
     core.info(`object locations - ${locations}`);
-    core.info(`metadata - ${metadata}`);
-    core.info(`metadata 2 - ${JSON.stringify(metadata)}`);
+    core.info(`metadata - ${METADATA}`);
     core.setOutput('object_key', destinationDir);
     core.setOutput('object_locations', locations);
   })
--- index.js
+++ index.js
@@ -33,29 +33,6 @@ const destinationDir = DESTINATION_DIR === '/' ? shortid() : DESTINATION_DIR;
 const paths = klawSync(SOURCE_DIR, {
   nodir: true
 });
-const metadata = getMap(METADATA);
-
-function getMap(jsonString){
-  var map = JSON.parse(jsonString, (key, value) =>{
-    var map = new Map();
-    map.set(key, value);
-    return map
-  });
-
-  return map;
-}
-
-// function dataReviver(key, value)
-// { 
-//     if(key == 'lastname')
-//     {
-//         var newLastname = "test";
-//         return newLastname;
-//     }
-
-//   return value;  // < here is where un-modified key/value pass though
-
-// }
 
 function upload(params) {
   return new Promise(resolve => {
@@ -78,7 +55,7 @@ function run() {
         Bucket: BUCKET,
         ACL: 'public-read',
         Body: fileStream,
-        Metadata: metadata,
+        Metadata: METADATA,
         Key: bucketPath,
         ContentType: lookup(p.path) || 'text/plain'
       };
@@ -91,8 +68,7 @@ run()
   .then(locations => {
     core.info(`object key - ${destinationDir}`);
     core.info(`object locations - ${locations}`);
-    core.info(`metadata - ${metadata}`);
-    core.info(`metadata 2 - ${JSON.stringify(metadata)}`);
+    core.info(`metadata - ${METADATA}`);
     core.setOutput('object_key', destinationDir);
     core.setOutput('object_locations', locations);
   })
